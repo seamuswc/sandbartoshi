325cc91a241cd8d65e5bc8496b93ed37c50c93b6
seamus <seamuswconnolly@gmail.com>
2024-12-12 20:10:42 +0900

    minor bugs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <title>Real Estate Listings</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/styles.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
